0a44db2aa902511fcac76d4ef59f9e2ca872bf43
Matheus Colombo da Cruz <matheus.colombo@autoglass.com.br>
2024-08-19 19:30:57 -0300

    Refatoração do método cronometrar

--- DesafioOrdenacao/Algoritmos/AlgoritmoOrdenacao.cs
+++ DesafioOrdenacao/Algoritmos/AlgoritmoOrdenacao.cs
@@ -8,21 +8,16 @@ public abstract class AlgoritmoOrdenacao
     //! Cronometra o tempo do ciclo de captura dos dados, ordenação e escrita dos dados
     public long Cronometrar(int quantidadeDeNumeros, string caminhoDeEntrada, string caminhoDeSaida)
     {
+        int[] vetor = FileHandler.ArquivoToArray(caminhoDeEntrada, quantidadeDeNumeros);
         Stopwatch cronometro = new();
 
         cronometro.Start();
-        EscreverArquivoPosOrdenacao(quantidadeDeNumeros, caminhoDeEntrada, caminhoDeSaida);
+        int[] vetorOrdenado = Sort(vetor);
         cronometro.Stop();
 
+        FileHandler.ArrayToArquivo(caminhoDeSaida, vetorOrdenado);
         return cronometro.ElapsedMilliseconds; 
-    }
-
-    //! Transforma o arquivo em um array e o array ordenado em um arquivo
-    private void EscreverArquivoPosOrdenacao(int quantidadeDeNumeros, string caminhoDeEntrada, string caminhoDeSaida)
-    {
-        int[] vetor = FileHandler.ArquivoToArray(caminhoDeEntrada, quantidadeDeNumeros);
-        FileHandler.ArrayToArquivo(caminhoDeSaida, Sort(vetor));
-    } 
+    }    
 
     //! Algoritmo de ordenação
     public virtual int[] Sort(int[] vetor)
--- DesafioOrdenacao/Program.cs
+++ DesafioOrdenacao/Program.cs
@@ -5,7 +5,7 @@ public class Program
 
     public static void Main(string[] args)
     {
-        int quantidadeDeNumeros = 500000;
+        int quantidadeDeNumeros = 50000;
         Bubble bubble = new();
         FileHandler.GerarArquivoComNumerosAleatorios("Files/teste.csv", quantidadeDeNumeros);
         Console.WriteLine($"Tempo de execução -> {bubble.Cronometrar(quantidadeDeNumeros, "Files/teste.csv", "Files/bubbleSort.csv")} Milisegundos");
