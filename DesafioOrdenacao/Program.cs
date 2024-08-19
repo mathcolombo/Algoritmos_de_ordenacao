964ee1f8a00122e8303a7bcbbc7887080d7f249e
Matheus Colombo da Cruz <matheus.colombo@autoglass.com.br>
2024-08-19 19:08:51 -0300

    Acerto

--- DesafioOrdenacao/Program.cs
+++ DesafioOrdenacao/Program.cs
@@ -5,13 +5,9 @@ public class Program
 
     public static void Main(string[] args)
     {
+        int quantidadeDeNumeros = 500000;
         Bubble bubble = new();
-        FileHandler.GerarArquivoComNumerosAleatorios("Files/teste.csv", 100000);
-        Console.WriteLine($"Tempo de execução -> {bubble.Cronometrar(100000, "Files/teste.csv", "Files/bubbleSort.csv")} Milisegundos");
-    }
-
-    private static void Menu()
-    {
-        
+        FileHandler.GerarArquivoComNumerosAleatorios("Files/teste.csv", quantidadeDeNumeros);
+        Console.WriteLine($"Tempo de execução -> {bubble.Cronometrar(quantidadeDeNumeros, "Files/teste.csv", "Files/bubbleSort.csv")} Milisegundos");
     }
 }
