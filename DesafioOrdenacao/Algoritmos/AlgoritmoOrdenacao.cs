eddec2cf0e381579adfab75471472620ffcfa834
Matheus Colombo da Cruz <matheus.colombo@autoglass.com.br>
2024-09-01 12:32:30 -0300

    Mudança de classe abstrata para Herança

--- DesafioOrdenacao/Algoritmos/AlgoritmoOrdenacao.cs
+++ DesafioOrdenacao/Algoritmos/AlgoritmoOrdenacao.cs
@@ -3,7 +3,7 @@ using DesafioOrdenacao.Handlers;
 
 namespace DesafioOrdenacao.Algoritmos;
 
-public abstract class AlgoritmoOrdenacao
+public class AlgoritmoOrdenacao
 {
     //! Cronometra o tempo do ciclo de captura dos dados, ordenação e escrita dos dados
     public long Cronometrar(int quantidadeDeNumeros, string caminhoDeEntrada, string caminhoDeSaida)
--- DesafioOrdenacao/Program.cs
+++ DesafioOrdenacao/Program.cs
@@ -6,8 +6,11 @@ public class Program
     public static void Main(string[] args)
     {
         int quantidadeDeNumeros = 50000;
+        string caminhoDeEntrada = "Files/teste.csv";
+        
         Bubble bubble = new();
+        
         FileHandler.GerarArquivoComNumerosAleatorios("Files/teste.csv", quantidadeDeNumeros);
-        Console.WriteLine($"Tempo de execução -> {bubble.Cronometrar(quantidadeDeNumeros, "Files/teste.csv", "Files/bubbleSort.csv")} Milisegundos");
+        Console.WriteLine($"Tempo de execução -> {bubble.Cronometrar(quantidadeDeNumeros, caminhoDeEntrada, "Files/bubbleSort.csv")} Milisegundos");
     }
 }
